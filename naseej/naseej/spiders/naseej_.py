a8ef6f75f52d4c3e1e580a43221a6523b5dd2d44
Mohamed-Elsafty23 <muhamad.elsafty@gmail.com>
2023-12-25 11:07:43 +0200

    Naseej scraping code

---
+++ naseej/naseej/spiders/naseej_.py
@@ -0,0 +1,38 @@
+import scrapy
+
+
+class NaseejSpider(scrapy.Spider):
+    name = 'naseej_'
+    allowed_domains = ['naseej.com']
+    start_urls = ['https://www.naseej.com/ar/news/page/1']
+
+    # check the number of pages from the website
+    number_of_pages = 2
+    def start_requests(self):
+        for i in range(1, self.number_of_pages + 1):
+            yield scrapy.Request(
+                url=f'https://www.naseej.com/ar/news/page/{i}',
+                callback=self.parse
+            )
+    def parse(self, response):
+        page_links = response.xpath('//h2[@itemprop="headline"]/a/@href').getall()
+        for link in page_links:
+            yield scrapy.Request(
+                url=link,
+                callback=self.parse_article
+            )
+
+    def parse_article(self, response):
+        title = response.xpath('//h1[@class="title"]/text()').get()
+        if title is None:
+            title= response.xpath('//p[@style="text-align: center;"]/span/strong/text()').get()
+        body = response.xpath('//div[@class="the_content_wrapper"]//text()').getall()
+        if body is None or len(body) == 0:
+            body = response.xpath('//p[@style="text-align: justify;"]/span/text()').getall()
+        body=''.join(body)
+        yield {
+            'url': response.url,
+            'title': title,
+            'date': response.xpath('//time[@itemprop="datePublished"]/text()').get(),
+            'body': body
+        }
