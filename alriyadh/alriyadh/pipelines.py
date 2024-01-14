dc0a76c718a79c0c67209270c60a7bd907e30fa2
Khalidalt <ekh@protonmail.com>
2024-01-14 22:06:33 +0300

    fix alriyadh

--- alriyadh/alriyadh/pipelines.py
+++ alriyadh/alriyadh/pipelines.py
@@ -8,6 +8,6 @@
 from itemadapter import ItemAdapter
 
 
-class TutorialPipeline:
+class AlriyadhPipeline:
     def process_item(self, item, spider):
         return item
--- alriyadh/alriyadh/spiders/alriyadh.py
+++ alriyadh/alriyadh/spiders/alriyadh.py
@@ -10,8 +10,8 @@ class Alriyadh(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.items = []
-        first_page = 897201
-        latest_page = 2045191
+        self.first_page = 897201
+        self.latest_page = 2045191
     def start_requests(self):
         for i in range(self.first_page,self.latest_page+1):  # Change to this after first page 897201 last page 1559429, before 2045191
             url = f'https://www.alriyadh.com/{i}'
@@ -40,6 +40,6 @@ class Alriyadh(scrapy.Spider):
 
     def closed(self, reason):
         # Save the chapters as a JSON file
-        json_path = 'alriyadh.json'
+        json_path = '/home/khalid/Documents/github_rep/MyProjects/scrap/alriyadh.json'
         with open(json_path, 'w', encoding='utf-8') as f:
             json.dump(self.items, f, ensure_ascii=False, indent=4)
